46ae470fbc267b93eb1e2b68e85adfb594a8e40f
Steve Tsourounis <stevetsourounis@gmail.com>
2014-03-26 00:43:38 -0400

    The 'last synced date' is now user specific (to handle multiple users per device)

--- src/ca/taglab/vocabnomad/rest/DataSyncRestService.java
+++ src/ca/taglab/vocabnomad/rest/DataSyncRestService.java
@@ -113,7 +113,7 @@ public class DataSyncRestService extends IntentService {
 
             // Get the date this object was last synced
             SharedPreferences settings = context.getSharedPreferences(TAG, 0);
-            mSyncDate = settings.getString(DATE_GET + this.mType, Contract.DEFAULT_DATE);
+            mSyncDate = settings.getString(UserManager.getUserId() + DATE_GET + this.mType, Contract.DEFAULT_DATE);
             Log.i(TAG, "Sync date: " + mSyncDate);
         }
 
@@ -194,7 +194,7 @@ public class DataSyncRestService extends IntentService {
                     SharedPreferences settings = mContext.getSharedPreferences(TAG, 0);
                     SharedPreferences.Editor editor = settings.edit();
                     SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd-HH-mm");
-                    editor.putString(DATE_GET + mType, format.format(new Date()));
+                    editor.putString(UserManager.getUserId() + DATE_GET + mType, format.format(new Date()));
                     editor.commit();
 
                     if (array.length() == 0 && mHandler != null) {
